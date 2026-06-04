c7249c56d4eb1681f375632fbac4c987309e5b4b
Jason Le <jason.le0804@gmail.com>
2026-06-04 17:44:48 -0500

    created model

--- Expense.java
+++ Expense.java
@@ -1,3 +1,45 @@
+import java.time.*;
+
 public class Expense {
+
+    final int id;
+    String itemName;
+    long itemCost;
+    LocalDate date;
+
+    Expense (int id, String itemName, long itemCost, LocalDate date) {
+        this.id = id;
+        this.itemName = itemName;
+        this.itemCost = itemCost;
+        this.date = date;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    String getItemName() {
+        return itemName;
+    }
+
+    long getItemCost() {
+        return itemCost;
+    }
+
+    LocalDate getDate() {
+        return date;
+    }
+
+    void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    void setItemCost(long itemCost) {
+        this.itemCost = itemCost;
+    }
+
+    void setDate(LocalDate date) {
+        this.date = date;
+    }
 
 }
