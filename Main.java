f76a52bdf474fc905ac69c21ea7335ed80d85c27
Jason Le <jason.le0804@gmail.com>
2026-06-04 23:41:26 -0500

    jtable ui

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.awt.BorderLayout;
+import java.awt.*;
 import javax.swing.*;
 
 public class Main {
@@ -7,15 +7,14 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Expense Tracker");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(1000,1000);
+            frame.setSize(750,1000);
 
-            JPanel expensesPanel = new JPanel();
+            JPanel expensesPanel = new JPanel(new BorderLayout());
             JPanel controlPanel = new JPanel();
             JButton createButton = new JButton("Create");
             JButton updateButton = new JButton("Update");
             JButton deleteButton = new JButton("Delete");
             JButton clearButton = new JButton("Clear");
-
             frame.add(expensesPanel, BorderLayout.CENTER);
             frame.add(controlPanel, BorderLayout.SOUTH);
             controlPanel.add(createButton);
@@ -23,9 +22,12 @@ public class Main {
             controlPanel.add(deleteButton);
             controlPanel.add(clearButton);
 
-            ExpenseJTable expenseJTable = new ExpenseJTable();
-
+            ExpenseManager expenseManager = new ExpenseManager();
+            ExpenseJTable expenseJTable = new ExpenseJTable(expenseManager);
+            
             JTable jTable = new JTable(expenseJTable);
+            JScrollPane scrollPane = new JScrollPane(jTable);
+            expensesPanel.add(scrollPane);
 
             frame.setVisible(true);
         });
