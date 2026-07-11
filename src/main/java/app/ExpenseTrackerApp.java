6b75c4e7a9beb30389511dc18a02caddef5bc643
Jason Le <jason.le0804@gmail.com>
2026-07-11 18:12:35 -0500

    created initial screen

--- src/main/java/app/ExpenseTrackerApp.java
+++ src/main/java/app/ExpenseTrackerApp.java
@@ -2,66 +2,45 @@ package app;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 
-import controller.ExpenseController;
-import model.ExpenseFilter;
-import service.ExpenseFilterService;
-import service.ExpenseService;
-import service.ExpenseTotalService;
-import ui.ControlPanel;
-import ui.ExpenseTable;
-import ui.FilterPanel;
-import ui.TotalPanel;
+import screens.ExpenseScreen;
 
 public class ExpenseTrackerApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args ) {
+
         SwingUtilities.invokeLater(() -> {
-
-            // Services + Model
-            ExpenseService expenseService = new ExpenseService();
-            ExpenseFilterService expenseFilterService = new ExpenseFilterService();
-            ExpenseTotalService expenseTotalService = new ExpenseTotalService();
-            ExpenseFilter expenseFilter = new ExpenseFilter("", "", true, true, null, null, null, null);
-
-            // TableModel + JTable
-            ExpenseTable expenseTable = new ExpenseTable(expenseFilterService, expenseService, expenseFilter);
-            JTable jTable = new JTable(expenseTable);
-
-            // Total Panel + Controller
-            TotalPanel totalPanel = new TotalPanel();
-            ExpenseController expenseController = new ExpenseController(expenseService, expenseTotalService, expenseTable, totalPanel);
-            
-            // Control + Filter Panels
-            ControlPanel controlPanel = new ControlPanel(jTable, expenseTable, expenseController);
-            FilterPanel filterPanel = new FilterPanel(expenseController, expenseFilter, expenseTable);
 
             // Initial Frame
             JFrame frame = new JFrame("Expense Tracker");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(1400, 1000);
+            frame.setSize(300, 75);
 
-            JScrollPane scrollPane = new JScrollPane(jTable);
-            JPanel expensePanel = new JPanel(new BorderLayout());
-            expensePanel.add(scrollPane);
+            // Button Panel
+            JPanel buttonPanel = new JPanel();
+            frame.add(buttonPanel, BorderLayout.CENTER);
+            JButton openExpenseButton = new JButton("Open Expense");
+            JButton createExpenseButton = new JButton("Create Expense");
+            buttonPanel.add(openExpenseButton);
+            buttonPanel.add(createExpenseButton);
 
-            JPanel centerPanel = new JPanel(new BorderLayout());
-            centerPanel.add(expensePanel, BorderLayout.CENTER);
-            centerPanel.add(totalPanel, BorderLayout.SOUTH);
+            // Button Functions
+            openExpenseButton.addActionListener(e -> {
+                frame.dispose();
+            });
 
-            frame.add(filterPanel, BorderLayout.NORTH);
-            frame.add(centerPanel, BorderLayout.CENTER);
-            frame.add(controlPanel, BorderLayout.SOUTH);
-
-            expenseTable.refreshView();
+            createExpenseButton.addActionListener(e -> {
+                frame.dispose();
+                ExpenseScreen.main(new String[] {});
+            });
 
             frame.setVisible(true);
         });
-    }
+
+    };
 
 }
---
+++ src/main/java/screens/ExpenseScreen.java
@@ -0,0 +1,67 @@
+package screens;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingUtilities;
+
+import controller.ExpenseController;
+import model.ExpenseFilter;
+import service.ExpenseFilterService;
+import service.ExpenseService;
+import service.ExpenseTotalService;
+import ui.ControlPanel;
+import ui.ExpenseTable;
+import ui.FilterPanel;
+import ui.TotalPanel;
+
+public class ExpenseScreen {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+
+            // Services + Model
+            ExpenseService expenseService = new ExpenseService();
+            ExpenseFilterService expenseFilterService = new ExpenseFilterService();
+            ExpenseTotalService expenseTotalService = new ExpenseTotalService();
+            ExpenseFilter expenseFilter = new ExpenseFilter("", "", true, true, null, null, null, null);
+
+            // TableModel + JTable
+            ExpenseTable expenseTable = new ExpenseTable(expenseFilterService, expenseService, expenseFilter);
+            JTable jTable = new JTable(expenseTable);
+
+            // Total Panel + Controller
+            TotalPanel totalPanel = new TotalPanel();
+            ExpenseController expenseController = new ExpenseController(expenseService, expenseTotalService, expenseTable, totalPanel);
+            
+            // Control + Filter Panels
+            ControlPanel controlPanel = new ControlPanel(jTable, expenseTable, expenseController);
+            FilterPanel filterPanel = new FilterPanel(expenseController, expenseFilter, expenseTable);
+
+            // Initial Frame
+            JFrame frame = new JFrame("Expense Tracker");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(1400, 1000);
+
+            JScrollPane scrollPane = new JScrollPane(jTable);
+            JPanel expensePanel = new JPanel(new BorderLayout());
+            expensePanel.add(scrollPane);
+
+            JPanel centerPanel = new JPanel(new BorderLayout());
+            centerPanel.add(expensePanel, BorderLayout.CENTER);
+            centerPanel.add(totalPanel, BorderLayout.SOUTH);
+
+            frame.add(filterPanel, BorderLayout.NORTH);
+            frame.add(centerPanel, BorderLayout.CENTER);
+            frame.add(controlPanel, BorderLayout.SOUTH);
+
+            expenseTable.refreshView();
+
+            frame.setVisible(true);
+        });
+    }
+
+}
